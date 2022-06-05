5c352634b0de1bd23c43d2cd5db5441954be4c38
hwisangcho00 <hwisangcho00@gmail.com>
2022-06-05 23:19:05 +0900

    index.html now reacts to app.js

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const rest_container = document.getElementById('restaurant');
+const get_rest_btn = document.getElementById('get_restaurant');
+
+get_rest_btn.addEventListener('click', () => createRestaurant());
+
+const createRestaurant = () => {
+  alert("hi");
+}
